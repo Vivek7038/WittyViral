5e254bd941fdb6553a06d42a769db05c7c55e751
Vivek7038 <vivek888chavan@gmail.com>
2024-01-22 15:09:23 +0530

    added loader

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom'
 const Navbar = () => {
+  const navigate=useNavigate();
   return (
    <>
-   <h1 className="text-4xl mt-4 mb-5 ">Meme Generator </h1>
+   <h1 className="text-4xl mt-4 mb-5 " onClick={()=>navigate("/")}>Meme Generator </h1>
    </>
   )
 }
--- src/pages/CreateMeme.jsx
+++ src/pages/CreateMeme.jsx
@@ -1,116 +1,131 @@
-import React, {useState} from "react";
-import {useLocation} from "react-router-dom"
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
 import InputsForm from "../helper/InputsForm.jsx";
 import Button from "@mui/material/Button";
-import {createMeme} from "../api/api";
+import { createMeme } from "../api/api";
 import DownloadMeme from "../helper/DownloadMeme.jsx";
-
+import CircularProgress from '@mui/material/CircularProgress';
 const fonts = [
-    {
-        value: 'impact',
-        label: 'impact',
-    },
-    {
-        value: 'arial',
-        label: 'arial',
-    },
+  {
+    value: "impact",
+    label: "impact",
+  },
+  {
+    value: "arial",
+    label: "arial",
+  },
 ];
 
 function CreateMeme() {
-    const location = useLocation();
-    const meme = location.state.meme;
-    const [generatedMeme, setGeneratedMeme] = useState("");
-    const [font, setFont] = useState("impact");
-    const [open, setOpen] = useState(false);
-    const [textBoxes, setTextBoxes] = useState({});
-    const handleClose = () => {
-        setOpen(!open);
-    };
-    let formData = new FormData();
-   
-    formData.append("template_id", meme.id);
-    formData.append("username", import.meta.env.VITE_REACT_APP_USER);
-    formData.append("password", import.meta.env.VITE_REACT_APP_PASSWORD);
-    formData.append("font", "impact");
+  const location = useLocation();
+  const meme = location.state.meme;
+  const [generatedMeme, setGeneratedMeme] = useState("");
+  const [font, setFont] = useState("impact");
+  const [open, setOpen] = useState(false);
+  const [textBoxes, setTextBoxes] = useState({});
+  const [loading, setLoading] = useState(false);
+  const handleClose = () => {
+    setOpen(!open);
+  };
+  let formData = new FormData();
 
+  formData.append("template_id", meme.id);
+  formData.append("username", import.meta.env.VITE_REACT_APP_USER);
+  formData.append("password", import.meta.env.VITE_REACT_APP_PASSWORD);
+  formData.append("font", "impact");
 
-    const changeForm = (key) => (event) => {
-        if (key === "font") {
-            setFont(event.target.value);
+  const changeForm = (key) => (event) => {
+    if (key === "font") {
+      setFont(event.target.value);
+    } else {
+      setTextBoxes({
+        ...textBoxes,
+        [key]: event.target.value,
+      });
+    }
+  };
+
+  const generateMeme = () => {
+    setLoading(true);
+    formData.set("font", font);
+    for (const key in textBoxes) {
+      formData.append(key, textBoxes[key]);
+    }
+    console.log(loading);
+    createMeme(formData)
+      .then((r) => {
+        if (r.success === true) {
+          setGeneratedMeme(r.data.url);
+          setLoading(false);
+          setOpen(true);
         } else {
-            setTextBoxes({
-                ...textBoxes, [key]: event.target.value,
-            });
+          console.log(r.error_message);
+          setLoading(false);
         }
-    };
+      })
+      .catch((e) => {
+        console.log(e)
+        setLoading(false)
+      });
+  };
 
-    const generateMeme = () => {
-     
-        formData.set("font", font);
-        for (const key in textBoxes) {
-            formData.append(key, textBoxes[key]);
-        }
-        createMeme(formData).then(r => {
-                if (r.success === true) {
-                    setGeneratedMeme(r.data.url);
-                    setOpen(true);
-                } else {
-                    console.log(r.error_message)
-                }
-            }
-        ).catch(e => console.log(e));
+  return (
+    <div className="content" style={{ right: 0, left: 0, margin: "auto" }}>
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        rowSpacing={1}
+        justify="center"
+      >
+        <Grid item xs={12} align="center">
+          <Typography variant={"h5"}>{meme.name}</Typography>
+        </Grid>
+        <Grid item xs={6} style={{ padding: "2%" }} align="center">
+         {loading && <CircularProgress />}
+         {!loading &&  <img height={350} src={meme.url} alt={meme.name} />}
+        </Grid>
+        <Grid item xs={4} style={{ padding: "2%" }} align="center">
+          <TextField
+            color="secondary"
+            style={{ width: "110px" }}
+            select
+            key="font"
+            id="font"
+            label="Select Font"
+            type="text"
+            value={font}
+            onChange={changeForm("font")}
+          >
+            {fonts.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
 
-    };
-
-    return (
-        <div className="content" style={{right: 0, left: 0, margin: "auto"}}>
-
-            <Grid container
-                  direction="row"
-                  alignItems="center"
-                  rowSpacing={1}
-                  justify="center">
-                <Grid item xs={12} align="center">
-                    <Typography variant={"h5"}>{meme.name}</Typography>
-                </Grid>
-                <Grid item xs={6} style={{padding: "2%"}} align="center">
-                    <img height={350} src={meme.url} alt={meme.name}/>
-                </Grid>
-                <Grid item xs={4} style={{padding: "2%"}} align="center">
-                    <TextField
-                        color="secondary"
-                        style={{width: "110px"}}
-                        select
-                        key="font"
-                        id="font"
-                        label="Select Font"
-                        type="text"
-                        value={font}
-                        onChange={changeForm("font")}
-                    >
-                        {fonts.map((option) => (
-                            <MenuItem key={option.value} value={option.value}>
-                                {option.label}
-                            </MenuItem>
-                        ))}
-                    </TextField>
-
-                    <InputsForm numOfFields={meme.box_count} changeForm={changeForm}/>
-                    <Button variant="outlined" color="secondary" onClick={generateMeme}>Generate</Button>
-
-                    {generatedMeme !== "" ?
-                        <DownloadMeme handleClose={handleClose} url={generatedMeme} open={open}/>
-                        : <></>}
-
-                </Grid>
-            </Grid>
-        </div>
-    )
-
+          <InputsForm numOfFields={meme.box_count} changeForm={changeForm} />
+          <Button variant="outlined" color="secondary" disabled={loading} onClick={generateMeme}>
+     Generate
+          </Button>
+        
+          {generatedMeme !== "" ? (
+            <DownloadMeme
+              handleClose={handleClose}
+              url={generatedMeme}
+              open={open}
+            />
+          ) : (
+            <></>
+          )}
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default CreateMeme;
